091f1544a8cca7f048ed96c99654caed75023628
Oscar Fernandez <oscar020606@gmail.com>
2025-02-06 19:10:52 -0400

    feat(c++): Test 02 - Algorithm01 method LightSync

---
+++ Test 2-01_2024/Algorithm01-test2.cpp
@@ -0,0 +1,47 @@
+/*
+You have a group of LED bulbs where each one turns on independently every few seconds (and then immediately turns off). 
+Write a C++ method called “lightSync”, which takes as input an array of integers representing the seconds it takes for each bulb to turn on and returns at what instant all the bulbs would turn on at the same time. 
+For example, for the array A[3] = (1,2,3); the result is 6.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int mcd(int a, int b){
+    while( b != 0){
+        int aux = b;
+        b = a % b;
+        a = aux;
+    }
+    return a;
+}
+
+int mcm(int a, int b){
+    return (a * b)/mcd(a, b);
+}
+
+int lightSync(int* arr, int n){
+    int result = arr[0];
+    for(int i = 1; i<n; i++){
+        result = mcm(result, arr[i]);
+    }
+    return result;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    int* arr = new int [n];
+    for(int i = 0; i < n; i++){
+        cin>> arr[i];
+    }
+
+    int seconds = lightSync(arr, n);
+
+    cout<<"Todos los bombillos se encenderan al mismo tiempo en "<<seconds<<" segundos";
+    delete [] arr;
+    return 0;
+}
